00a8f61cb5d062c9cc1c22f1c6b61c27a70f4bfe
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-04-18 16:29:00 +0200

    [refine] style of offcanvas element

--- server.php
+++ server.php
@@ -3,7 +3,7 @@ $json_string = file_get_contents('album-list.json');
 $album_list = json_decode($json_string, true);
 
 // Logic to add a new album: at least there must be a title of the new album
-if (isset($_POST['newAlbumTitle'])) {
+if (!empty($_POST['newAlbumTitle'])) {
   $newAlbumToAdd =  [
     'title' => $_POST['newAlbumTitle'],
     'author' => $_POST['newAlbumAuthor'],
